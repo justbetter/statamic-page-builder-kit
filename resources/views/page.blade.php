80170564c272131fedc4485a2405f486b7dd76f1
Kevin Meijer <kevin@justbetter.nl>
2024-11-20 14:20:29 +0100

    Removed old code

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 
-{{--@section('title', $meta_title->raw() ? $meta_title .' - '. config('app.name') : config('app.name'))--}}
-{{--@section('meta_description', (isset($meta_description) && !empty($meta_description->raw())) ? $meta_description : config('app.name'))--}}
-
 @section('content')
     @include('statamic-page-builder-kit::page_builder')
 @endsection
